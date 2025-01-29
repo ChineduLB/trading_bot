aaf31c939c9cb0c5a209aea172a0d13942174e3e
Chinedu Okoli <46028203+ChineduLB@users.noreply.github.com>
2025-01-29 11:29:19 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,40 @@
+# Module Attributes
+SELL = "SELL"
+BUY = "BUY"
+ACTIVE = "active"
+COMPLETED = "completed"
+DESCRIPTION = 'description'
+TICKER = 'ticker'
+KUCOIN = 'KUCOIN'
+COINBASE = 'COINBASE'
+IGNORE = "IGNORE"
+KUCOIN_LbCrypto3000 = "LbCrypto3000"
+KUCOIN_LbCrypto2000 = "LbCrypto2000"
+KUCOIN_LbCrypto4000 = "LbCrypto4000"
+KuCoin_LbCrypto5000KC = "LbCrypto5000KC"
+KuCoin_LbCrypto7000KC = "LbCrypto7000KC"
+KuCoin_LbCrypto8000 = "LbCrypto8000"
+KuCoin_LbCrypto1000 = "LbCrypto1000"
+KuCoin_LbCrypto9000KC = "LbCrypto9000KC"
+
+TRADE_HISTORY_TBL = "ugochukwu_trade_details" #crypto_trade_details"
+# Ugochukwu_Trade_Details = "ugochukwu_trade_details"
+KuCoin_ACCOUNTS = [KUCOIN, KUCOIN_LbCrypto3000, KUCOIN_LbCrypto2000, KUCOIN_LbCrypto4000,
+                   KuCoin_LbCrypto5000KC,KuCoin_LbCrypto7000KC, KuCoin_LbCrypto8000,
+                   KuCoin_LbCrypto1000, KuCoin_LbCrypto9000KC]
+LbCrypto2000Cb = "LbCrypto2000Cb"
+LbCrypto1000Cb = "LbCrypto1000Cb"
+LbCrypto4000Cb = "LbCrypto4000Cb"
+LbCrypto5000Cb = "LbCrypto5000Cb"
+LbCrypto7000Cb = "LbCrypto7000Cb"
+LbCrypto8000Cb = "LbCrypto8000Cb"
+LbCrypto9000Cb = "LbCrypto9000Cb"
+LbCrypto1100Cb = "LbCrypto1100Cb"
+LbCrypto1200Cb = "LbCrypto1200Cb"
+ActiveSellExist = "Active Trade exists for sale"
+CheckExistingTrade = "CheckExistingTrade"
+GetAmountResult = "GetAmountResult"
+OrderDetail = "order_detail"
+SaveBuyOrderResult = "SaveBuyOrderResult"
+BuyMarketOrderTickers = ['BTC/USDT', 'ETH/USDT', 'MATIC/USD', 'KCS/USDT', 'BAT/USDT', 'ONE/USDT', 'ADA/USD', 'DOGE/USDT']
+SellMarketOrderTickers = []
